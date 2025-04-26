7ff9a193ec4e776f1b1a0b8c8b3e4a5398ad7273
tadtd <tiendat1243@gmail.com>
2025-04-27 00:48:10 +0700

    upload p01b

--- ps1/src/p01b_logreg.py
+++ ps1/src/p01b_logreg.py
@@ -3,7 +3,6 @@ import util
 
 from linear_model import LinearModel
 
-
 def main(train_path, eval_path, pred_path):
     """Problem 1(b): Logistic regression with Newton's Method.
 
@@ -15,6 +14,14 @@ def main(train_path, eval_path, pred_path):
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
 
     # *** START CODE HERE ***
+    model = LogisticRegression(eps=1e-5)
+    model.fit(x_train, y_train)
+
+    util.plot(x_train, y_train, model.theta, 'output/p01b_{}.png'.format(pred_path[-5]))
+
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    y_pred = model.predict(x_eval)
+    np.savetxt(pred_path, y_pred, fmt='%d')
     # *** END CODE HERE ***
 
 
@@ -34,8 +41,23 @@ class LogisticRegression(LinearModel):
             x: Training example inputs. Shape (m, n).
             y: Training example labels. Shape (m,).
         """
-        # *** START CODE HERE ***
-        # *** END CODE HERE ***
+        m, n = x.shape
+        self.theta = np.zeros(n)
+
+        #Newton's Method
+        while True:
+            old_theta = np.copy(self.theta)
+            # Compute the hypothesis
+            h = 1 / (1 + np.exp(-x.dot(self.theta)))
+            # Compute the Hessian matrix
+            hessian = x.T.dot(np.diag(h * (1 - h))).dot(x) / m
+            # Compute the gradient
+            gradient = x.T.dot(h - y) / m
+            # Update theta
+            self.theta -= np.linalg.inv(hessian).dot(gradient)
+            # Check for convergence
+            if np.linalg.norm(old_theta - self.theta, ord=1) < self.eps:
+                break
 
     def predict(self, x):
         """Make a prediction given new inputs x.
@@ -47,4 +69,5 @@ class LogisticRegression(LinearModel):
             Outputs of shape (m,).
         """
         # *** START CODE HERE ***
+        return 1 / (1 + np.exp(-x.dot(self.theta))) > 0.5
         # *** END CODE HERE ***
